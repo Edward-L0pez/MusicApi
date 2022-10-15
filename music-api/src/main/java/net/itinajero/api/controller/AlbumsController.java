3fd1282bb043246b48dfa9da483d44e41d18ad14
Edward-L0pez <lopezealz@gmail.com>
2022-10-15 13:00:43 -0500

    Subiendo los cambios del Video 10-12 de la carpeta 15

--- music-api/src/main/java/net/itinajero/api/controller/AlbumsController.java
+++ music-api/src/main/java/net/itinajero/api/controller/AlbumsController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
 import net.itinajero.api.entity.Album;
 import net.itinajero.api.service.IAlbumsService;
@@ -21,6 +26,24 @@ public class AlbumsController {
 	public List<Album> buscarTodos(){
 		return serviceAlbums.buscarTodos();
 
+	}
+	
+	@PostMapping("/albums") 
+	public Album guardar(@RequestBody Album album) {
+		serviceAlbums.guardar(album);
+		return album;
+	}
+
+	@PutMapping("/albums")
+	public Album modificar(@RequestBody Album album) {
+		serviceAlbums.guardar(album); //Guardando en la base de datos
+		return album;
+	} 
+
+	@DeleteMapping("/albums/{id}")
+	public String eliminar(@PathVariable("id") int idAlbum) {
+		serviceAlbums.eliminar(idAlbum);
+		return "Registro Eliminado";
 	}
 
 }
